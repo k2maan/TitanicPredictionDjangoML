f5453cc80b9e7479cc0febc954dfc1f9f76a33ba
k2maan <ketuman333@gmail.com>
2020-10-27 14:26:30 +0530

    Added CSS styling

--- titanic/base/views.py
+++ titanic/base/views.py
@@ -1,26 +1,29 @@
 from django.shortcuts import render
+import pickle
 
-def home(request):    
+
+def home(request):
     return render(request, 'index.html')
 
 
 def getPredictions(pclass, sex, age, sibsp, parch, fare, C, Q, S):
 
-    import pickle
-
     model = pickle.load(open("ml_model.sav", "rb"))
     scaled = pickle.load(open("scaler.sav", "rb"))
-    prediction = model.predict(scaled.transform([[pclass, sex, age, sibsp, parch, fare, C, Q, S]]))
-    
+
+    prediction = model.predict(scaled.transform(
+        [[pclass, sex, age, sibsp, parch, fare, C, Q, S]]))
+
     if prediction == 0:
-        return "not survived"
+        return "no"
     elif prediction == 1:
-        return "survived"
+        return "yes"
     else:
-        return "error"
-        
+        return "Some error occured."
+
 
 def result(request):
+
     pclass = int(request.GET['pclass'])
     sex = int(request.GET['sex'])
     age = int(request.GET['age'])
@@ -31,8 +34,7 @@ def result(request):
     embQ = int(request.GET['embQ'])
     embS = int(request.GET['embS'])
 
-    result = getPredictions(pclass, sex, age, sibsp, parch, fare, embC, embQ, embS)
+    result = getPredictions(pclass, sex, age, sibsp,
+                            parch, fare, embC, embQ, embS)
 
-    return render(request, 'result.html', {'result':result})
-
-    
+    return render(request, 'result.html', {'result': result})
